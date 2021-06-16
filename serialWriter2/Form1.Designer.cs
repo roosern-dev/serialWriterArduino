d1bc21e13745a35c8e3af51d1940665baae367eb
Roo <rs.yeoh@estechs.com>
2021-06-16 22:52:59 +0800

    new commit

---
+++ serialWriter2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace serialWriter2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HighButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LowButton = new System.Windows.Forms.Button();
+            this.FrequencyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HighButton
+            // 
+            this.HighButton.Location = new System.Drawing.Point(346, 203);
+            this.HighButton.Name = "HighButton";
+            this.HighButton.Size = new System.Drawing.Size(75, 23);
+            this.HighButton.TabIndex = 0;
+            this.HighButton.Text = "HIGH";
+            this.HighButton.UseVisualStyleBackColor = true;
+            this.HighButton.Click += new System.EventHandler(this.HighButton_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start";
+            // 
+            // LowButton
+            // 
+            this.LowButton.Location = new System.Drawing.Point(346, 233);
+            this.LowButton.Name = "LowButton";
+            this.LowButton.Size = new System.Drawing.Size(75, 23);
+            this.LowButton.TabIndex = 2;
+            this.LowButton.Text = "LOW";
+            this.LowButton.UseVisualStyleBackColor = true;
+            this.LowButton.Click += new System.EventHandler(this.LowButton_Click);
+            // 
+            // FrequencyButton
+            // 
+            this.FrequencyButton.Location = new System.Drawing.Point(346, 263);
+            this.FrequencyButton.Name = "FrequencyButton";
+            this.FrequencyButton.Size = new System.Drawing.Size(75, 23);
+            this.FrequencyButton.TabIndex = 3;
+            this.FrequencyButton.Text = "FREQ";
+            this.FrequencyButton.UseVisualStyleBackColor = true;
+            this.FrequencyButton.Click += new System.EventHandler(this.FrequencyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FrequencyButton);
+            this.Controls.Add(this.LowButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HighButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button HighButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LowButton;
+        private System.Windows.Forms.Button FrequencyButton;
+    }
+}
+
---
+++ serialWriter2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace serialWriter2
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialHandler = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.serialHandler.PortName = "COM3";
+            this.serialHandler.BaudRate = 9600;
+        }
+
+
+        private void HighButton_click(object sender, EventArgs e)
+        {
+            this.label1.Text = "HIGH";
+            this.serialHandler.Open();
+            this.serialHandler.WriteLine("H");
+            this.serialHandler.Close();
+            
+        }
+
+        private void LowButton_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "LOW";
+            this.serialHandler.Open();
+            this.serialHandler.WriteLine("L");
+            this.serialHandler.Close();
+        }
+
+        private void FrequencyButton_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "FREQUENCY";
+            this.serialHandler.Open();
+            this.serialHandler.WriteLine("F");
+            this.serialHandler.Close();
+        }
+    }
+}
